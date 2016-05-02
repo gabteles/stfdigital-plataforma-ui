ac76e9039b18c5480228f12d528d794365bb7632
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-05-02 20:10:27 -0300

    Adicionando a constante de apiUrl.

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -2,9 +2,13 @@
 {
     'use strict';
 
+    var url = "http://docker";
+    var port = "8765";
+
     angular.module('app.constants', []).constant("properties", {
-        "url": "http://docker",
-        "port": "8765",
+        "url": url,
+        "port": port,
+        "apiUrl": url + ":" + port,
         "development": true
     });
     

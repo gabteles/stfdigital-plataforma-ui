30bf0314a9da4cbae15ee251bacfe151face92ac
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-05-11 18:26:48 -0300

    Recuperando o host da UI dinamicamente para conseguir rodar no servidor.

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -2,7 +2,7 @@
 {
     'use strict';
 
-    var url = "http://docker";
+    var url = "http://" + (window.location.hostname !== 'localhost' ? window.location.hostname : 'docker');
     var port = "8765";
 
     angular.module('app.constants', []).constant("properties", {

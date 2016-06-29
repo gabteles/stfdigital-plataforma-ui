3b30cd89e3a8a2aa590421ae5da7ba49b17e584a
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-06-29 13:16:57 -0300

    Removendo classes que foram pro mock do teste e2e.

--- definitions/certification/converter.service.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace app.certification {
-    class ConverterService {
-        base64ToArrayBuffer(base64: any): ArrayBuffer;
-        arrayBufferToBase64(buffer: any): string;
-        hex2ArrayBuffer(str: any): ArrayBuffer;
-        arrayBuffer2hex(args: any): string;
-    }
-}

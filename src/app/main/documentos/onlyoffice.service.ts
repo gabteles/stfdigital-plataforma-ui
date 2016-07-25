22dbe35f1c80ed91c50ad72530f06683eb7595af
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-25 12:51:42 -0300

    Repassando autenticação para o onlyoffice.

--- src/app/main/documentos/onlyoffice.service.ts
+++ src/app/main/documentos/onlyoffice.service.ts
@@ -4,13 +4,15 @@ namespace app.documentos {
 	import IHttpService = angular.IHttpService;
 	import IQService = angular.IQService;
 	import IPromise = angular.IPromise;
+	import ICookies = angular.cookies.ICookiesService;
 
 	export class OnlyofficeService {
 		
 		private baseUrl: string;
 
 		/** @ngInject **/
-		constructor(private $http: IHttpService, private $q : IQService, private properties) { }
+		constructor(private $http: IHttpService, private $q : IQService, private properties: app.support.constants.Properties,
+			private $cookies: ICookies) { }
 
 		private getBaseUrl(): IPromise<string> {
 			if (!this.baseUrl) {
@@ -31,27 +33,39 @@ namespace app.documentos {
 			}
 		}
 		
+		private accessToken(): string {
+			return this.$cookies.get('access_token');
+		}
+
+		private accessTokenParam(): string {
+			return 'access_token=' + this.accessToken();
+		}
+
+		private csrfParam(): string {
+			return '_csrf=' + this.accessToken();
+		}
+
 		public criarUrlConteudoDocumento(id: number) {
-			return this.getBaseUrl().then(function(baseUrl) {
-				return baseUrl + '/documents/api/onlyoffice/documentos/' + id + '/conteudo.docx';
+			return this.getBaseUrl().then((baseUrl) => {
+				return baseUrl + '/documents/api/onlyoffice/documentos/' + id + '/conteudo.docx?' + this.accessTokenParam();
 			});
 		}
 
 		public gerarNumeroEdicao(id: number) : IPromise<any> {
-			return this.$http.put(this.properties.apiUrl + '/documents/api/onlyoffice/documentos/' + id + '/edicao', null).then(function(response) {
+			return this.$http.put(this.properties.apiUrl + '/documents/api/onlyoffice/documentos/' + id + '/edicao', null).then((response) => {
 				return response.data;
 			});
 		}
 		
 		public recuperarNumeroEdicao(id: number): IPromise<any> {
-			return this.$http.get(this.properties.apiUrl + '/documents/api/onlyoffice/documentos/' + id + '/edicao').then(function(response) {
+			return this.$http.get(this.properties.apiUrl + '/documents/api/onlyoffice/documentos/' + id + '/edicao').then((response) => {
 				return response.data;
 			});
 		}
 		
 		recuperarUrlCallback(id: number) {
-			return this.$q.when(this.getBaseUrl()).then(function(baseUrl) {
-				return baseUrl + '/documents/api/onlyoffice/documentos/' + id + '/callback';
+			return this.$q.when(this.getBaseUrl()).then((baseUrl) => {
+				return baseUrl + '/documents/api/onlyoffice/documentos/' + id + '/callback?' + this.accessTokenParam() + '&' + this.csrfParam();
 			});
 		}
 		

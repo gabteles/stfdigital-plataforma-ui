214d793a41cd57ac08f35b99d93b638f113557ef
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-26 20:52:13 -0300

    Diretiva stf-processing.

---
+++ src/app/main/core/directives/stf-processing/ng-submit.decorator.ts
@@ -0,0 +1,33 @@
+namespace app.core {
+
+	/** @ngInject */
+	function configNgSbumitDecorator($provide: ng.auto.IProvideService) {
+		$provide.decorator('ngSubmitDirective', ($delegate, $timeout: ng.ITimeoutService) => {
+			let originalCompile = $delegate[0].compile;
+			$delegate[0].compile = (element, attrs, transclude) => {
+				if (angular.isString(attrs.stfProcessing)) {
+					let alreadyMadeRequest = false;
+					function handleDuplicateRequests(event) {
+						if (alreadyMadeRequest) {
+							event.preventDefault();
+							event.stopImmediatePropagation();
+						} else {
+							alreadyMadeRequest = true;
+							$timeout(() => {alreadyMadeRequest = false}, 500, false);
+						}
+					}
+					element.on('submit', handleDuplicateRequests);
+					attrs['ngSubmit'] = '$stfDecorateNgSubmitHandler(' + attrs['ngSubmit'] + ')';
+					return originalCompile(element, attrs, transclude);
+				} else {
+					return originalCompile(element, attrs, transclude);
+				}
+			};
+			
+			return $delegate;
+		});
+	}
+
+	angular.module('app.core').config(configNgSbumitDecorator);
+
+}

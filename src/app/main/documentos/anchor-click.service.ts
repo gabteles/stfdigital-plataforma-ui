e325c3dd8845f06e5972496a4c85a26ab4e600ea
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-10-28 20:27:36 -0200

    Reformatando os tabs para espaços em branco.

--- src/app/main/documentos/anchor-click.service.ts
+++ src/app/main/documentos/anchor-click.service.ts
@@ -1,23 +1,23 @@
 namespace app.documentos {
 
-	/**
-	 * Service auxiliar utilizado para tornar testável
-	 * o carregamento do documento PDF em uma nova aba.
-	 * 
-	 * Sem essa delegação do clique, não seria possível
-	 * verificar esse carregamento.
-	 */
-	export class AnchorClickService {
+    /**
+     * Service auxiliar utilizado para tornar testável
+     * o carregamento do documento PDF em uma nova aba.
+     * 
+     * Sem essa delegação do clique, não seria possível
+     * verificar esse carregamento.
+     */
+    export class AnchorClickService {
 
-		/**
-		 * Realiza o clique no elemento anchor.
-		 */
-		public clickAnchor(a: HTMLAnchorElement) {
-			a.click();
-		}
+        /**
+         * Realiza o clique no elemento anchor.
+         */
+        public clickAnchor(a: HTMLAnchorElement) {
+            a.click();
+        }
 
-	}
+    }
 
-	angular.module("app.documentos").service("app.documentos.AnchorClickService", AnchorClickService);
+    angular.module("app.documentos").service("app.documentos.AnchorClickService", AnchorClickService);
 
 }
--- test/unit/app/main/documentos/visualizador-basico.directive.spec.ts
+++ test/unit/app/main/documentos/visualizador-basico.directive.spec.ts
@@ -1,83 +1,83 @@
 namespace app.documentos {
 
-	describe("Teste da diretiva visualizador-basico", () => {
+    describe("Teste da diretiva visualizador-basico", () => {
 
-		/**
-		 * Escopo que simula a situação típica em que
-		 * a diretiva será utilizada.
-		 */
-		interface TestScope extends ng.IScope {
-			vm: {
-				apiParaAcessarODocumento;
-			};
-		}
+        /**
+         * Escopo que simula a situação típica em que
+         * a diretiva será utilizada.
+         */
+        interface TestScope extends ng.IScope {
+            vm: {
+                apiParaAcessarODocumento;
+            };
+        }
 
-		const API_PARA_ACESSAR_DOCUMENTO = "/api/para/acessar-o-documento";
-		const PDF_CONTENT = "%PDF-1.3\nAAA\n30 0 obj\n%%EOF333";
+        const API_PARA_ACESSAR_DOCUMENTO = "/api/para/acessar-o-documento";
+        const PDF_CONTENT = "%PDF-1.3\nAAA\n30 0 obj\n%%EOF333";
 
-		let $compile: ng.ICompileService;
-		let $rootScope: ng.IRootScopeService;
-		let scope: TestScope;
-		let element: ng.IAugmentedJQuery;
-		let template;
+        let $compile: ng.ICompileService;
+        let $rootScope: ng.IRootScopeService;
+        let scope: TestScope;
+        let element: ng.IAugmentedJQuery;
+        let template;
 
-		let anchorClickService: AnchorClickService;
+        let anchorClickService: AnchorClickService;
 
-		let $httpBackend: ng.IHttpBackendService;
+        let $httpBackend: ng.IHttpBackendService;
 
-		beforeEach(() => {
-			angular.mock.module("app.core");
+        beforeEach(() => {
+            angular.mock.module("app.core");
 
-			angular.module("app.autenticado", []);
-			angular.mock.module("app.support.constants");
-			angular.module("app.support", ["app.support.constants"]);
+            angular.module("app.autenticado", []);
+            angular.mock.module("app.support.constants");
+            angular.module("app.support", ["app.support.constants"]);
 
-			angular.mock.module("ngMockE2E", "templates", "app.documentos");
-		});
+            angular.mock.module("ngMockE2E", "templates", "app.documentos");
+        });
 
-		beforeEach(inject(["app.documentos.AnchorClickService", (_anchorClickService_: AnchorClickService) => {
-			anchorClickService = _anchorClickService_;
-		}]));
+        beforeEach(inject(["app.documentos.AnchorClickService", (_anchorClickService_: AnchorClickService) => {
+            anchorClickService = _anchorClickService_;
+        }]));
 
-		beforeEach(inject((_$compile_: ng.ICompileService, _$rootScope_: ng.IRootScopeService, _$httpBackend_: ng.IHttpBackendService) => {
-			$compile = _$compile_;
-			$httpBackend = _$httpBackend_;
-			$rootScope = _$rootScope_;
+        beforeEach(inject((_$compile_: ng.ICompileService, _$rootScope_: ng.IRootScopeService, _$httpBackend_: ng.IHttpBackendService) => {
+            $compile = _$compile_;
+            $httpBackend = _$httpBackend_;
+            $rootScope = _$rootScope_;
 
-			$httpBackend.whenGET('http://docker:8765/documents/api/onlyoffice/baseUrl').respond(200, "http://onlyoffice");
+            $httpBackend.whenGET('http://docker:8765/documents/api/onlyoffice/baseUrl').respond(200, "http://onlyoffice");
 
-			scope = <TestScope>_$rootScope_.$new();
+            scope = <TestScope>_$rootScope_.$new();
 
-			scope.vm = {
-				apiParaAcessarODocumento: API_PARA_ACESSAR_DOCUMENTO
-			};
+            scope.vm = {
+                apiParaAcessarODocumento: API_PARA_ACESSAR_DOCUMENTO
+            };
 
-			element = angular.element(`<a href="#" stf-visualizador-basico="vm.apiParaAcessarODocumento"></a>`);
-			template = $compile(element)(scope);
-			$rootScope.$digest();
-		}));
+            element = angular.element(`<a href="#" stf-visualizador-basico="vm.apiParaAcessarODocumento"></a>`);
+            template = $compile(element)(scope);
+            $rootScope.$digest();
+        }));
 
-		it("Deveria carregar o documento da api", () => {
-			$httpBackend.expectGET(API_PARA_ACESSAR_DOCUMENTO).respond(200, PDF_CONTENT);
+        it("Deveria carregar o documento da api", () => {
+            $httpBackend.expectGET(API_PARA_ACESSAR_DOCUMENTO).respond(200, PDF_CONTENT);
 
-			element.triggerHandler("click");
+            element.triggerHandler("click");
 
-			$httpBackend.verifyNoOutstandingExpectation();
-		});
+            $httpBackend.verifyNoOutstandingExpectation();
+        });
 
-		it("Deveria carregar o documento em uma nova janela do navegador", () => {
-			$httpBackend.expectGET(API_PARA_ACESSAR_DOCUMENTO).respond(200, PDF_CONTENT);
+        it("Deveria carregar o documento em uma nova janela do navegador", () => {
+            $httpBackend.expectGET(API_PARA_ACESSAR_DOCUMENTO).respond(200, PDF_CONTENT);
 
-			element.triggerHandler("click");
+            element.triggerHandler("click");
 
-			spyOn(anchorClickService, "clickAnchor").and.callFake((a: HTMLAnchorElement) => {
-				expect(a.target).toBe("_blank");
-				expect(a.href).toBeTruthy();
-			});
+            spyOn(anchorClickService, "clickAnchor").and.callFake((a: HTMLAnchorElement) => {
+                expect(a.target).toBe("_blank");
+                expect(a.href).toBeTruthy();
+            });
 
-			$httpBackend.flush();
-		});
+            $httpBackend.flush();
+        });
 
-	});
+    });
 
 }
